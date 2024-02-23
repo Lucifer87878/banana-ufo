53023fd6998b56649f3ef103727e6dad980a00a4
Lucifer <nallenrockar123@gmail.com>
2024-02-23 09:50:44 +0100

    tilläg av information om alla UFO's

--- src/Components/Ufo-List/ufolist.jsx
+++ src/Components/Ufo-List/ufolist.jsx
@@ -11,19 +11,39 @@ function UfoList() {
                     <li className='card'>
                     <div class="container">
                         <h4><b>Millennium Falcon</b></h4>
-                        <p>Star Ship</p>
+                        <p>StarShip Class	
+                            YT-1300F light freighter / smuggling ship</p>
                         </div>
                     </li>
                     <li className='card'>
                     <div class="container">
                         <h4><b>USS Enterprise</b></h4>
-                        <p>Star Ship</p>
+                        <p>Star Ship
+                        Registry: USS Enterprise (NCC-1701-A)
+                        Class: Constitution II
+                        Service: 2286–2293 (7 years)
+                        Captains: James T. Kirk (William Shatner)
+                        This ship first appears at the conclusion of Star Trek IV: The Voyage Home (1986) 
+                        and is the main setting in the subsequent Star Trek movies which use the original crew. 
+                        The ship is ordered "decommissioned" at the end of Star Trek VI: The Undiscovered Country (1991).[6] 
+                        As of 2401, it resides at the Starfleet Museum above Athan Prime, appearing in season 3 of Star Trek: Picard (2023). 
+                        Non-canon information concerning this ship includes paperwork included with the model kit, 
+                        which indicated the ship was mothballed at the Memory Alpha ship museum, 
+                        and the Shatnerverse novel The Ashes of Eden (1996),
+                        which depicted Enterprise-A's removal from the mothball fleet before being destroyed defending the planet Chal.
+                        </p>
                         </div>
                     </li>
                     <li className='card'>
                     <div class="container">
                         <h4><b>TARDIS</b></h4>
-                        <p>Magic gate</p>
+                        <p>Magic gate
+                        ‘TARDIS’ stands for ‘Time and Relative Dimension in Space’ – an acronym coined by Susan, 
+                        the Doctor’s granddaughter. A Type 40 capsule crafted by the Time Lords, 
+                        the TARDIS is the Doctor’s incredible ship for travelling in time and space. 
+                        They stole it from Gallifrey in their youth, 
+                        using it to run away and embark on adventures across the universe.
+                        </p>
                         </div>
                     </li>
                     <li className='card'>
@@ -37,37 +57,58 @@ function UfoList() {
                     <li className='card'>
                     <div class="container">
                         <h4><b>Discovery One</b></h4>
-                        <p>Star Ship</p>
+                        <p>The United States Spacecraft Discovery One is a fictional spaceship featured in the first two novels of the Space Odyssey series 
+                            by Arthur C. Clarke and in the films 2001: A Space Odyssey (1968) directed by Stanley Kubrick and 2010: The Year We Make Contact (1984) directed by Peter Hyams. 
+                            The ship is a nuclear-powered interplanetary spaceship, crewed by two men and controlled by the AI on-board computer HAL 9000. 
+                            The ship is destroyed in the second novel and makes no further appearances.</p>
                         </div>
                     </li>
                     <li className='card'>
                     <div class="container">
                         <h4><b>Nostromo</b></h4>
-                        <p>Star Ship</p>
+                        <p>The USCSS Nostromo, registration number 1809246(09),
+                            [3] was a modified Lockmart CM-88B Bison M-Class star freighter owned by the Weyland-Yutani 
+                            Corporation and commanded by Captain Arthur Dallas, 
+                            registered out of Panama.[2] The Nostromo operated as a tug, 
+                            connecting to and pulling loads like a tractor truck 
+                            rather than carrying those loads on board like a traditional freighter.</p>
                         </div>
                     </li>
                     <li className='card'>
                     <div class="container">
                         <h4><b>Battlestar Galactica</b></h4>
-                        <p>Star Ship</p>
+                        <p>Battlestar Galactica is a fictional space battleship and one of an unspecified number of carrier type warcraft constructed by the Twelve Colonies of Man, 
+                            the Battlestar Galactica is the flag vessel of the colonial planet Caprica. 
+                            Galactica was launched more than 500 years before the close of the Thousand Yahren War which concluded during the events of the series pilot episode.[1] 
+                            It was believed to be the only Battlestar to survive the destruction of the Twelve Colonies, until Battlestar Pegasus briefly reappeared. 
+                            The Galactica is commanded by Commander Adama.</p>
                         </div>
                     </li>
                     <li className='card'>
                     <div class="container">
                         <h4><b>Event Horizon</b></h4>
-                        <p>Star Ship</p>
+                        <p>The Event Horizon was commissioned and launched in the year 2040 as a platform to test the experimental Gravity Drive developed by Dr. 
+                            Willam Weir. It was the largest space faring vessel built up to that point, measuring almost 2200 metres in length. 
+                            At its core lies the gravity drive which allows the ship to travel terrific distances in moments by "folding space" and creating a "gateway". 
+                            The ship was designed by Dr Weir and built to be able to travel "faster than light".</p>
                         </div>
                     </li>
                     <li className='card'>
                     <div class="container">
                         <h4><b>Sulaco</b></h4>
-                        <p>Star Ship</p>
+                        <p>USS Sulaco was the 13th starship of the Conestoga class operated by the United States Colonial Marines.[1] 
+                            Sulaco held a reputation as the most unlucky Conestoga ship in the fleet, being involved in docking accidents, 
+                            fires, attacks and a record number of friendly fire incidents.</p>
                         </div>
                     </li>
                     <li className='card'>
                     <div class="container">
                         <h4><b>Heart of Gold</b></h4>
-                        <p>Star Ship</p>
+                        <p>The starship Heart of Gold was the first spacecraft to make use of the Infinite Improbability Drive. 
+                            The craft was stolen by then-President Zaphod Beeblebrox at the official launch of the ship, 
+                            as he was supposed to be officiating the launch. Later, during the use of the Infinite Improbability Drive, 
+                            the ship picked up Arthur Dent and Ford Prefect, who were floating unprotected in deep space in the same star sector, 
+                            having just escaped the destruction of the same planet.</p>
                         </div>
                     </li>
                 </ul>
